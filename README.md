3dda43b8f1f48fc9ce6580865aa6faeb9caa0ebd
turalalv <cybermanexo@gmail.com>
2020-09-24 17:20:24 +0300

    APK open source

---
+++ README.md
@@ -0,0 +1,35 @@
+# ApkUrlGrep
+Tool that allow extract endpoints from APK files
+![alt text](https://i.ibb.co/V3nFRwJ/image-2020-05-06-21-30-30.png)
+
+## Install
+1) Install `apkurlgrep`
+```
+▶ go get -u github.com/ndelphit/apkurlgrep
+```
+2) Install [apktool](https://ibotpeaches.github.io/Apktool/install/)
+
+
+## Usage
+
+
+```
+▶ apkurlgrep -a ~/path/to/file.apk
+Result of URLs:
+
+https://example.com
+https://example.net
+https://example.edu
+
+Result of URLs Paths:
+
+/example
+/admin
+/onboarding
+```
+## Ideas
+Hey folks! 🙂
+
+Thank you for the interesting of this project! If you found any ideas about how to improve this tool - please write me a message on whoareme@wearehackerone.com or create new issues in this Github Project.
+
+Have a nice day! 😉
---
+++ apksour.go
@@ -0,0 +1,38 @@
+/*
+ made with love by @Legendhot 9/2020
+*/
+
+package main
+
+import (
+	"fmt"
+	"github.com/akamensky/argparse"
+	"github.com/ndelphit/apkurlgrep/command/apktool"
+	dependency "github.com/ndelphit/apkurlgrep/command/dependency"
+	"github.com/ndelphit/apkurlgrep/directory"
+	"github.com/ndelphit/apkurlgrep/extractor"
+	"os"
+)
+
+
+
+func main() {
+
+	parser := argparse.NewParser("apksour", "ApkSouR - Extract endpoints from APK files")
+	apk := parser.String("a", "apk", &argparse.Options{Required: true, Help: "Input a path to APK file."})
+
+	err := parser.Parse(os.Args)
+
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		os.Exit(-1)
+	}
+
+	var baseApk = *apk
+	var tempDir = directory.CreateTempDir()
+
+	dependency.AreAllReady()
+	apktool.RunApktool(baseApk, tempDir)
+	extractor.Extract(tempDir)
+	directory.RemoveTempDir(tempDir)
+}
